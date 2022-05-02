204f7402cacd6de76cd0c0a676155a1d224e4919
Lucky <1107744081@qq.com>
2022-05-02 10:57:18 +0800

    优化

--- self_play.py
+++ self_play.py
@@ -22,97 +22,104 @@ while 1==1:
         np_board = myboard.getNp()
         np_board = np_board + myboard.player
         move_str = myboard.getNpList()
-        if myboard.player == 'r':
-            other_side = 'b'
+        if move_str == None or move_str == '':
+            move_rec.append(myboard.getNp()+myboard.player)
+            result = myboard.player+"输"
         else:
-            other_side = 'r'
-        move_arra = move_str.split(',')
-        while '' in move_arra:
-            move_arra.remove('')
-        move_c = move_arra[0]
-        search_str_c = r.hget(move_c+other_side, 'search_count')
-        value_str_c = r.hget(move_c+other_side,'value')
-        if value_str_c != None:
-            value_c = int(value_str_c)
-        else:
-            value_c = 0
-        if search_str_c != None:
-            search_c = int(search_str_c)
-        else:
-            search_c = 0
-        for move_tmp in move_arra:
-            value_str = r.hget(move_tmp+other_side,'value')
-            search_str = r.hget(move_tmp + other_side, 'search_count')
-            if None == search_str:
-                search =0
+            if myboard.player == 'r':
+                other_side = 'b'
             else:
-                search= int(search_str)
-            if None != value_str:
-                value = int(value_str)
+                other_side = 'r'
+            move_arra = move_str.split(',')
+            while '' in move_arra:
+                move_arra.remove('')
+            move_c = move_arra[0]
+            search_str_c = r.hget(move_c+other_side, 'search_count')
+            value_str_c = r.hget(move_c+other_side,'value')
+            if value_str_c != None:
+                value_c = int(value_str_c)
             else:
-                value = 0
-            if value-search > value_c-search_c:
-                move_c = move_tmp
-                value_c = value
+                value_c = 0
+            if search_str_c != None:
+                search_c = int(search_str_c)
+            else:
+                search_c = 0
+            for move_tmp in move_arra:
+                value_str = r.hget(move_tmp+other_side,'value')
+                search_str = r.hget(move_tmp + other_side, 'search_count')
+                if None == search_str:
+                    search =0
+                else:
+                    search= int(search_str)
+                if None != value_str:
+                    value = int(value_str)
+                else:
+                    value = 0
+                # print('目标flag')
+                # print(value_c-search_c)
+                if value-search > value_c-search_c:
+                    move_c = move_tmp
+                    value_c = value
             move_rec.append(myboard.getNp()+myboard.player)
             result = myboard.takeActionbyNp(move_c)
+
     if result == 'r输':
         for arra in move_rec:
             # 检索次数更新
             search_str = r.hget(arra, 'search_count')
             if None != search_str:
-                search = int(search_str) + 1
+                search = int(search_str) + 11
                 r.hset(arra, 'search_count', str(search))
             if None == search_str:
-                r.hset(arra, 'search_count', '1')
+                r.hset(arra, 'search_count', '11')
             # 价值更新
             if 'b' in arra:
                 value_str = r.hget(arra, 'value')
                 if None != value_str:
-                    value = int(value_str)-1
+                    value = int(value_str)-10
                     r.hset(arra, 'value',str(value))
                 if None == value_str:
-                    r.hset(arra, 'value', '-1')
+                    r.hset(arra, 'value', '-10')
             if 'r' in arra:
                 value_str = r.hget(arra, 'value')
                 if None != value_str:
-                    value = int(value_str)+1
+                    value = int(value_str)+10
                     r.hset(arra, 'value',str(value))
                 if None == value_str:
-                    r.hset(arra, 'value', '1')
+                    r.hset(arra, 'value', '10')
     if result == 'b输':
         for arra in move_rec:
             # 检索次数更新
             search_str = r.hget(arra, 'search_count')
             if None != search_str:
-                search = int(search_str) + 1
+                search = int(search_str) + 11
                 r.hset(arra, 'search_count', str(search))
             if None == search_str:
-                r.hset(arra, 'search_count', '1')
+                r.hset(arra, 'search_count', '11')
             # 价值更新
             if 'b' in arra:
                 value_str = r.hget(arra, 'value')
                 if None != value_str:
-                    value = int(value_str)+1
+                    value = int(value_str)+10
                     r.hset(arra, 'value',str(value))
                 if None == value_str:
-                    r.hset(arra, 'value', '1')
+                    r.hset(arra, 'value', '10')
             if 'r' in arra:
                 value_str = r.hget(arra, 'value')
                 if None != value_str:
-                    value = int(value_str)-1
+                    value = int(value_str)-10
                     r.hset(arra, 'value',str(value))
                 if None == value_str:
-                    r.hset(arra, 'value', '-1')
+                    r.hset(arra, 'value', '-10')
     if result == '和':
         for arra in move_rec:
             # 检索次数更新
             search_str = r.hget(arra, 'search_count')
             if None != search_str:
-                search = int(search_str) + 1
+                search = int(search_str) + 11
                 r.hset(arra, 'search_count', str(search))
             if None == search_str:
-                r.hset(arra, 'search_count', '1')
+                r.hset(arra, 'search_count', '11')
         value_count = 0
         for cross in myboard.crosses:
             if cross.piece !=None:
@@ -144,37 +151,37 @@ while 1==1:
                     value_count -= 1000
                 if cross.piece.pieceId == 7 and cross.piece.side =='b':
                     value_count -= 1
-        if value_count>3:
+        if value_count>=2:
             for arra in move_rec:
                 # 价值更新
                 if 'b' in arra:
                     value_str = r.hget(arra, 'value')
                     if None != value_str:
-                        value = int(value_str) + 1
+                        value = int(value_str) + 10
                         r.hset(arra, 'value', str(value))
                     if None == value_str:
-                        r.hset(arra, 'value', '1')
+                        r.hset(arra, 'value', '10')
                 if 'r' in arra:
                     value_str = r.hget(arra, 'value')
                     if None != value_str:
-                        value = int(value_str) - 1
+                        value = int(value_str) - 10
                         r.hset(arra, 'value', str(value))
                     if None == value_str:
-                        r.hset(arra, 'value', '-1')
-        if value_count<3:
+                        r.hset(arra, 'value', '-10')
+        if value_count<=-2:
             for arra in move_rec:
                 # 价值更新
                 if 'b' in arra:
                     value_str = r.hget(arra, 'value')
                     if None != value_str:
-                        value = int(value_str) - 1
+                        value = int(value_str) - 10
                         r.hset(arra, 'value', str(value))
                     if None == value_str:
-                        r.hset(arra, 'value', '-1')
+                        r.hset(arra, 'value', '-10')
                 if 'r' in arra:
                     value_str = r.hget(arra, 'value')
                     if None != value_str:
-                        value = int(value_str) + 1
+                        value = int(value_str) + 10
                         r.hset(arra, 'value', str(value))
                     if None == value_str:
-                        r.hset(arra, 'value', '1')
+                        r.hset(arra, 'value', '10')
