81734ee0630c4b8285ee19c2987d9e1c17c646c5
Lucky <1107744081@qq.com>
2022-05-02 10:58:33 +0800

    优化

--- game.py
+++ game.py
@@ -28,25 +28,53 @@ while 1==1:
             print(move)
         myboard.takeAction(move)
     else:
+        if myboard.player == 'r':
+            other_side = 'b'
+        else:
+            other_side = 'r'
         print('电脑预测......')
         np_board = myboard.getNp()
-        move_str = r.hget(np_board+myboard.player,'c_node')
+        move_str = myboard.getNpList()
         if move_str == None:
             print('电脑认输')
             break
         move_arra = move_str.split(',')
         move_c = move_arra[0]
-        value_str_c = r.hget(move_c,'value')
+        value_str_c = r.hget(move_c+other_side,'value')
         if value_str_c != None:
             value_c = int(value_str_c)
         else:
             value_c = 0
         for move_tmp in move_arra:
-            value_str = r.hget(move_tmp,'value')
+            value_str = r.hget(move_tmp+other_side,'value')
             if None != value_str:
                 value = int(value_str)
                 if value > value_c:
                     move_c = move_tmp
                     value_c = value
         print(move_c)
+        is_expert = r.hget(move_c+other_side,'is_expert')
+        print(is_expert)
         myboard.takeActionbyNp(move_c)
+        mat = "{strtmp: ^{len}}"
+        for cross in myboard.crosses:
+            if (cross.rx % 9 == 0):
+                if (cross.piece != None):
+                    strtmp = cross.piece.name
+                    if cross.piece.side == 'r':
+                        print("\033[0;37;41m" + mat.format(strtmp=strtmp, len=5) + "\033[0m")
+                    else:
+                        print("\033[0;37;43m" + mat.format(strtmp=strtmp, len=5) + "\033[0m")
+                else:
+                    strtmp = "口"
+                    print(mat.format(strtmp=strtmp,len=5))
+            else:
+                if (cross.piece != None):
+                    strtmp = cross.piece.name
+                    if cross.piece.side == 'r':
+                        print("\033[0;37;41m"+mat.format(strtmp=strtmp,len=5)+"\033[0m", end="")
+                    else:
+                        print("\033[0;37;43m" + mat.format(strtmp=strtmp, len=5) + "\033[0m", end="")
+                else:
+                    strtmp = "口"
+                    print(mat.format(strtmp=strtmp, len=5), end="")
